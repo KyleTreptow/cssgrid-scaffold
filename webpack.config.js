9667e5d6c516cd8a1da992dcc58558b91ca6489c
Bryan Lau <bryan.j.lau@gmail.com>
2018-03-14 21:16:33 -0700

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: {
+        grid: './entry/grid.js'
+    },
+    output: {
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.sass$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        extractSass
+    ]
+};
